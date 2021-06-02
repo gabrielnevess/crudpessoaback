492db515b0fe4b10f333902757fda063a53b9564
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2021-06-01 22:05:07 -0300

    first commit

---
+++ src/test/java/com/example/crudpessoa/CrudpessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudpessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
